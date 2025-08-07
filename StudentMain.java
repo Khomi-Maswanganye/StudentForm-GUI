d37fc39d958c9c25b433ad447f5e17a3dbf4eac4
K N Maswanganye <Khomananimokoena@gmail.com>
2025-08-07 11:21:39 +0200

    The main class, java code for the form and form

---
+++ StudentMain.java
@@ -0,0 +1,16 @@
+
+package studentmain;
+import student.form.za.StudentForm;
+
+
+public class StudentMain {
+
+    
+    public static void main(String[] args) {
+      
+        StudentForm sf = new StudentForm();
+        
+        sf.setVisible(true);
+    }
+    
+}
